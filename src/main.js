563cd1841416fb8fc6e7e99d7aedeea193c5b3f4
Евгения <zhoonya1@rambler.ru>
2020-04-18 18:46:37 +0300

    Добавлены обработчик для Esc и отрисовка страницы без событий.

--- src/main.js
+++ src/main.js
@@ -6,36 +6,66 @@ import TripSortComponent from "./components/trip-sort.js";
 import TripDaysListComponent from "./components/trip-days-list.js";
 import TripDayComponent from "./components/trip-day.js";
 import EventComponent from "./components/event.js";
-import {generateEvents} from "./mock/event.js";
 import EditEventComponent from "./components/edit-event.js";
+import NoEventsComponent from "./components/no-events.js";
+import {generateEvents} from "./mock/event.js";
 import {getListOfDates} from "./mock/trip-day.js";
 import {render, RenderPosition} from "./utils.js";
 
 const EVENT_COUNT = 10;
 
 const renderEvent = (tripEventsList, event) => {
-  const onOpenButtonClick = () => {
+  const replaceEventToEdit = () => {
     tripEventsList.replaceChild(editEventComponent.getElement(), eventComponent.getElement());
   };
-  const onCloseButtonClick = (evt) => {
-    evt.preventDefault();
+  const replaceEditToEvent = () => {
     tripEventsList.replaceChild(eventComponent.getElement(), editEventComponent.getElement());
+  };
+
+  const EscKeyDownHandler = (evt) => {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      replaceEditToEvent();
+      document.removeEventListener(`keydown`, EscKeyDownHandler);
+    }
   };
 
   const eventComponent = new EventComponent(event);
   const openButton = eventComponent.getElement().querySelector(`.event__rollup-btn`);
-  openButton.addEventListener(`click`, onOpenButtonClick);
+  openButton.addEventListener(`click`, () => {
+    replaceEventToEdit();
+    document.addEventListener(`keydown`, EscKeyDownHandler);
+  });
 
   const editEventComponent = new EditEventComponent(event);
   const editForm = editEventComponent.getElement().querySelector(`.event--edit`);
   const closeButton = editEventComponent.getElement().querySelector(`.event__rollup-btn`);
-  editForm.addEventListener(`submit`, onCloseButtonClick);
-  closeButton.addEventListener(`click`, onCloseButtonClick);
+  editForm.addEventListener(`submit`, (evt) => {
+    evt.preventDefault();
+    replaceEditToEvent();
+    document.removeEventListener(`keydown`, EscKeyDownHandler);
+  });
+  closeButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    replaceEditToEvent();
+    document.removeEventListener(`keydown`, EscKeyDownHandler);
+  });
 
   render(tripEventsList, eventComponent.getElement(), RenderPosition.BEFOREEND);
 };
 
-const renderTripDays = (tripDaysList, dates, events) => {
+const renderTripDays = (tripEventsElement, dates, events) => {
+  if (events.length === 0) {
+    render(tripEventsElement, new NoEventsComponent().getElement(), RenderPosition.BEFOREEND);
+    return;
+  }
+
+  render(tripEventsElement, new TripSortComponent().getElement(), RenderPosition.BEFOREEND);
+
+  const tripDaysList = new TripDaysListComponent();
+  render(tripEventsElement, tripDaysList.getElement(), RenderPosition.BEFOREEND);
+
   for (let i = 0; i < dates.length; i++) {
     const tripDayComponent = new TripDayComponent(i + 1, dates[i]);
     render(tripDaysList.getElement(), tripDayComponent.getElement(), RenderPosition.BEFOREEND);
@@ -58,7 +88,6 @@ const dates = getListOfDates(events);
 const siteHeaderElement = document.querySelector(`.trip-main`);
 const menuElement = siteHeaderElement.querySelector(`#header-menu`);
 const filtersElement = siteHeaderElement.querySelector(`#header-filters`);
-const tripEventsElement = document.querySelector(`.trip-events`);
 
 render(menuElement, new SiteMenuComponent().getElement(), RenderPosition.AFTEREND);
 render(filtersElement, new FilterComponent().getElement(), RenderPosition.AFTEREND);
@@ -67,9 +96,7 @@ render(siteHeaderElement, new TripInfoComponent(dates, events).getElement(), Ren
 const tripInfoElement = siteHeaderElement.querySelector(`.trip-info`);
 render(tripInfoElement, new TripInfoCostComponent(events).getElement(), RenderPosition.BEFOREEND);
 
-render(tripEventsElement, new TripSortComponent().getElement(), RenderPosition.BEFOREEND);
+const tripEventsElement = document.querySelector(`.trip-events`);
 
-const tripDaysList = new TripDaysListComponent();
-render(tripEventsElement, tripDaysList.getElement(), RenderPosition.BEFOREEND);
-renderTripDays(tripDaysList, dates, events);
+renderTripDays(tripEventsElement, dates, events);
 
